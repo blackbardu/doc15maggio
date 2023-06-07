71df2e64308780a2cbc07546286030320660a173
blackbardu <rickbardu@gmail.com>
2023-06-07 21:17:54 +0200

    info added -> dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -10,57 +10,72 @@ const socket = io.connect('http://localhost:3001');
 const Dashboard = () => {
   const [filePresence, setFilePresence] = useState({});
   const { myArray } = useContext(MyArrayContext);
-  const filteredArray = myArray.filter(item => item !== 'coordinatore');
-  
+  const filteredArray = myArray.filter((item) => item !== 'coordinatore');
+
+  const renderErrorMessage = (filename) => {
+    return (
+      <div>
+        <span style={{ color: 'red', fontSize: '12px' }}>
+          Il file "{filename}" non è presente/non è stato inserito contenuto!
+        </span>
+      </div>
+    );
+  };
+
+  const renderSuccessMessage = (filename) => {
+    return (
+      <div>
+        <span style={{ color: 'green', fontSize: '12px' }}>
+          Il file "{filename}" è stato caricato correttamente!
+        </span>
+      </div>
+    );
+  };
 
   useEffect(() => {
     socket.emit('dotted_files', { myArray: filteredArray });
 
     socket.on('filepresence', (data) => {
-        const { filename, isPresent } = data;
-        setFilePresence((prevFilePresence) => ({
-          ...prevFilePresence,
-          [filename]: isPresent,
-        }));
-      });
+      const { filename, isPresent } = data;
+      setFilePresence((prevFilePresence) => ({
+        ...prevFilePresence,
+        [filename]: isPresent,
+      }));
+    });
 
-
-    socket.on('filedownload', ({filename})=>{
-        alert(filename)
-    })
-    
+    socket.on('filedownload', ({ filename }) => {
+      alert(filename);
+    });
 
     return () => {
       socket.off('filepresence');
     };
   }, []);
 
-  
-    const isFilePresent = (filename) => {
-      return filePresence[filename] || false;
-    };
+  const isFilePresent = (filename) => {
+    return filePresence[filename] || false;
+  };
 
-    const isBothFilesPresent = (item) => {
-        const programmasvolto = `programmasvolto_${item}.txt`;
-        const relazionefinale = `relazionefinale_${item}.txt`;
-    
-        return (
-          isFilePresent(programmasvolto) && isFilePresent(relazionefinale)
-        );
-    };
+  const isBothFilesPresent = (item) => {
+    const programmasvolto = `programmasvolto_${item}.txt`;
+    const relazionefinale = `relazionefinale_${item}.txt`;
 
-    const downloadFile = (filename) => {
-        const downloadUrl = `http://localhost:3001/download/${filename}`;
-        window.open(downloadUrl);
-      };
+    return (
+      isFilePresent(programmasvolto) && isFilePresent(relazionefinale)
+    );
+  };
 
-    const richiediDocumento = (pageName) => {
-        socket.emit('downloadFile', { pageName });
-        const filename = `output_${pageName}.pdf`;
-        downloadFile(filename);
-      };
+  const downloadFile = (filename) => {
+    const downloadUrl = `http://localhost:3001/download/${filename}`;
+    window.open(downloadUrl);
+  };
 
-    
+  const richiediDocumento = (pageName) => {
+    socket.emit('downloadFile', { pageName });
+    const filename = `output_${pageName}.pdf`;
+    downloadFile(filename);
+  };
+
   return (
     <div>
       <div className="title">Dashboard</div>
@@ -70,14 +85,23 @@ const Dashboard = () => {
             <h3>{item.charAt(0).toUpperCase() + item.slice(1)}</h3>
           ) : (
             <div>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <h3>{item.charAt(0).toUpperCase() + item.slice(1)}</h3>
-                    {isBothFilesPresent(item) && (
-                    <Button variant="primary" onClick={() => richiediDocumento(item)} style={{ marginLeft: '10px' }}>
-                        <FaFileAlt /> 
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <h3>{item.charAt(0).toUpperCase() + item.slice(1)}</h3>
+                {isBothFilesPresent(item) && (
+                  <>
+                    <Button
+                      variant="primary"
+                      onClick={() => richiediDocumento(item)}
+                      style={{ marginLeft: '10px' }}
+                    >
+                      <FaFileAlt style={{ verticalAlign: 'middle', }} />
                     </Button>
-                    )}
-                </div>
+                    <span style={{ color: 'blue', fontSize: '12px', marginLeft: '5px' }}>
+                      Scarica il documento
+                    </span>
+                  </>
+                )}
+              </div>
               <ul>
                 <li>
                   Relazione finale{' '}
@@ -85,6 +109,12 @@ const Dashboard = () => {
                     <span style={{ color: 'green' }}>&#9679;</span>
                   ) : (
                     <span style={{ color: 'red' }}>&#9679;</span>
+                  )}
+                  {!isFilePresent(`relazionefinale_${item}.txt`) && (
+                    renderErrorMessage(`relazionefinale_${item}.txt`)
+                  )}
+                  {isFilePresent(`relazionefinale_${item}.txt`) && (
+                    renderSuccessMessage(`relazionefinale_${item}.txt`)
                   )}
                 </li>
 
@@ -95,8 +125,13 @@ const Dashboard = () => {
                   ) : (
                     <span style={{ color: 'red' }}>&#9679;</span>
                   )}
+                  {!isFilePresent(`programmasvolto_${item}.txt`) && (
+                    renderErrorMessage(`programmasvolto_${item}.txt`)
+                  )}
+                  {isFilePresent(`programmasvolto_${item}.txt`) && (
+                    renderSuccessMessage(`programmasvolto_${item}.txt`)
+                  )}
                 </li>
-                
               </ul>
             </div>
           )}
